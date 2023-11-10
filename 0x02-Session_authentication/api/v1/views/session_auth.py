decb9bdc4b57e6130019e57634c599a051fadca3
aanito <howmarad@gmail.com>
2023-11-10 06:00:33 +0300

    db auths

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,38 +1,54 @@
 #!/usr/bin/env python3
-"""class SessionAuth"""
-from api.v1.auth.auth import Auth
-import uuid
+"""This module contains views for session authentication.
+"""
+from typing import Tuple
 from models.user import User
+from flask import abort, jsonify, request
+import os
+from api.v1.views import app_views
 
 
-class SessionAuth(Auth):
-    """SessionAuth"""
-    user_id_by_session_id = {}
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> Tuple[str, int]:
+    """Log in a user using session authentication.
+    Returns:
+        Tuple containing a JSON representation of a User object and an
+        HTTP status code.
+    """
+    not_found_res = {"error": "no user found for this email"}
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or len(email.strip()) == 0:
+        return jsonify({"error": "email missing"}), 400
+    if password is None or len(password.strip()) == 0:
+        return jsonify({"error": "password missing"}), 400
+    try:
+        users = User.search({'email': email})
+    except Exception:
+        return jsonify(not_found_res), 404
+    if len(users) <= 0:
+        return jsonify(not_found_res), 404
+    if users[0].is_valid_password(password):
+        from api.v1.app import auth
+        sessiond_id = auth.create_session(getattr(users[0], 'id'))
+        res = jsonify(users[0].to_json())
+        res.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
+        return res
+    return jsonify({"error": "wrong password"}), 401
 
-    def create_session(self, user_id: str = None) -> str:
-        """creates a Session ID for a user_id"""
-        if user_id is None:
-            return None
-        if type(user_id) != str:
-            return None
-        id = str(uuid.uuid4())
-        self.user_id_by_session_id[id] = user_id
-        return id
 
-    def user_id_for_session_id(self,
-                               session_id: str = None) -> str:
-        """ a User ID based on a Session ID"""
-        if session_id is None:
-            return None
-        if type(session_id) != str:
-            return None
-        return self.user_id_by_session_id.get(session_id)
+@app_views.route(
+    '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout() -> Tuple[str, int]:
+    """
+    Logs out the current user by destroying their session.
 
-    def current_user(self, request=None):
-        """Returns a User instance based on a cookie value"""
-        session_cookie = self.session_cookie(request)
-        try:
-            user_id = self.user_id_by_session_id[session_cookie]
-            return User.get(user_id)
-        except Exception as NotFound:
-            return None
+    Returns:
+        A tuple containing an empty JSON response and a status code of 200.
+        If the session could not be destroyed, returns a 404 error.
+    """
+    from api.v1.app import auth
+    is_destroyed = auth.destroy_session(request)
+    if not is_destroyed:
+        abort(404)
+    return jsonify({})
